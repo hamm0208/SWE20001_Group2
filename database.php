c4d63c2088b21302f9fff28b9585c8ecfe9c1d33
hamm0208 <107385845+hamm0208@users.noreply.github.com>
2024-03-29 20:04:50 +0800

    Update database.php

--- database.php
+++ database.php
@@ -15,4 +15,14 @@ mysqli_query($conn, $sql);
 //Select FoodEdge database
 mysqli_select_db($conn, $dbname);
 
+$sql = "CREATE TABLE IF NOT EXISTS inventory(
+    id INT(4) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    category VARCHAR(50),
+    price DECIMAL(10, 2),
+    image_name VARCHAR(255)
+)";
+mysqli_query($conn, $sql);
+
 ?>
