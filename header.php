10361ed11f974cb398940466533ae2a00c8dae6d
ajnlyn <121025481+ajnlyn@users.noreply.github.com>
2024-04-05 02:23:47 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="FoodEge, catering, food, beverage">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Anjanaa Lyan AP Nagulan, Beckham Then">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/style.css"/>
+    <title>Menu</title>
+</head>
+<body>
+    <!-- Navbar (sit on top) -->
+    <nav>
+        <div class="nav-content">
+            <div class="FoodEdge-logo">
+                <a href="index.html">
+                    <img src="images/web_resources/FoodEdgeLogo_NoBG.png" width="150" height="150" alt="FoodEdgeLogo_NoBG" title="FoodEdgeLogo_NoBG">
+                </a>
+            </div>
+            <div class="right-align">
+                <ul class="nav-links">
+                    <li class="dropdown">
+                        <a href="#" class="dropdownbutton">CATERING MENU</a>
+                        <div class="dropdown-content">
+                            <a href="foodmenu.php">FOOD</a>
+                            <a href="beveragemenu.php">BEVERAGE</a>
+                        </div>
+                    </li>
+                    <li><a href="#">ORDERS</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                    <li><a href="#">MY PROFILE</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ menu.php
@@ -0,0 +1,49 @@
+<?php include 'header.php'; ?>
+
+<section class="container1">
+    <div><h1 class="category" id="cateringmenu">CATERING MENU<span class="menu"></span></h1></div>
+    <section class="card-container">
+        <section class="card">
+            <figure class="front1">
+                <img src="images/food_image/deluxecheeseburger.jpg" alt="Image 1">
+                <figcaption class="front-wording1">Food</figure>
+            <div class="back">
+                <div class="back-wording">
+                    <h5 class="info-title">
+                        Food Menu
+                        <br>
+                        <br>
+                        Delight in a culinary journey through our diverse menu offerings, each dish prepared with passion and precision to tantalize your taste buds. 
+                        <br>
+                        Have fun browsing!
+                        <br>
+                        <br>
+                        <a href="foodmenu.html" class="start-browsing-button">Start Browsing</a>
+                    </h5>
+                </div>
+            </div>
+        </section>
+        <section class="card">
+            <figure class="front1">
+                <img src="images/food_image/freshfruitsmoothie.jpg" alt="Image 1">
+                <figcaption class="front-wording1">Beverage</figure>
+            <div class="back">
+                <div class="back-wording">
+                    <h5 class="info-title">
+                        Beverage Menu
+                        <br>
+                        <br>
+                        Explore a tantalizing array of flavors on our beverage menu, crafted to quench every thirst and elevate every moment.
+                        <br>
+                        Have fun browsing!  
+                        <br>
+                        <br>
+                        <a href="foodmenu.html" class="start-browsing-button">Start Browsing</a>
+                    </h5>
+                </div>
+            </div>
+        </section>
+    </section>
+</section> 
+
+<?php include 'footer.php'; ?>
