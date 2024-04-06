ddf903de7a4db773362eb52c5eebd7cec5bf8c48
hamm0208 <107385845+hamm0208@users.noreply.github.com>
2024-04-06 17:37:14 +0800

    Update for header and footer

--- menu.php
+++ menu.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="FoodEge, catering, food, beverage">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Anjanaa Lyan AP Nagulan, Beckham Then">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <title>Menu</title>
+</head>
+
 <?php include 'header.php'; ?>
 
 <section class="container1">
@@ -47,3 +63,5 @@
 </section> 
 
 <?php include 'footer.php'; ?>
+</body>
+</html>
