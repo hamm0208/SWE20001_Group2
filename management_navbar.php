680fd2cfde1b30aa0df7f9a76705d6ef447c83c6
hamm0208 <107385845+hamm0208@users.noreply.github.com>
2024-05-04 00:12:16 +0800

    Update management_navbar.php

--- management_navbar.php
+++ management_navbar.php
@@ -16,6 +16,16 @@
     <div class="nav mt-3 d-flex justify-content-center">
         <ul class="list-inline">
             <li class="list-inline-item">
+                <a href="management_packages.php" class='linkToManagementAddItem'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="50" viewBox="0 -960 960 960" width="50"><path d="M440-600v-120H320v-80h120v-120h80v120h120v80H520v120h-80ZM280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM40-800v-80h131l170 360h280l156-280h91L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68.5-39t-1.5-79l54-98-144-304H40Z"/></svg>
+                    <p class="playfair-display h4">Item Packages</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="nav mt-3 d-flex justify-content-center">
+        <ul class="list-inline">
+            <li class="list-inline-item">
                 <a href="management_orders.php" class='linkToManagementAddItem'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="#000000" height="50" viewBox="0 -960 960 960" width="50"><path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z"/></svg>    
                     <p class="playfair-display h4">Orders</p>
