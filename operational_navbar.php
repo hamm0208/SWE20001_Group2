0fbf602864978d334671772f1e6cf8f507eeb280
hamm0208 <107385845+hamm0208@users.noreply.github.com>
2024-05-09 02:22:41 +0800

    update order status

---
+++ operational_navbar.php
@@ -0,0 +1,26 @@
+<div class="col-2 mt-3 my-3  text-center nav_management">
+    <a href='operational_updateorderstatus.php'>
+        <img src="Images/web_resources/foodEdge_logo.png" alt="FoodEdge logo" class="logo img-fluid w-75">
+    </a>
+    <br>
+    <div class="nav mt-5 d-flex justify-content-center">
+        <ul class="list-inline ">
+            <li class="list-inline-item">
+                <a href="operational_updateorderstatus.php" class='linkToManagementAddItem'>
+                    <img src="Images/web_resources/inventory_logo.png" alt="Inventory Logo" class='link_logo w-25'>
+                    <p class="playfair-display h4">Update Orders</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="nav mt-3 d-flex justify-content-center">
+        <ul class="list-inline">
+            <li class="list-inline-item">
+                <a href="log_out.php" class='linkToManagementAddItem'>
+                    <img src="Images/web_resources/sigout_img.png" alt="Log out Logo" class='link_logo w-25'>
+                    <p class="playfair-display h4">Log Out</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
